bdd8b1e42d2ec93188f141af52d1c65c62c3637e
tianyasky <1960446107@qq.com>
2026-05-13 14:30:12 +0800

    Rebuild Android Side soon later

--- Application/app/src/main/java/com/tianya/application/data/model/UploadCompleteResponse.java
+++
@@ -1,8 +0,0 @@
-package com.tianya.application.data.model;
-
-public class UploadCompleteResponse {
-    private String videoUrl;
-
-    public String getVideoUrl() { return videoUrl; }
-    public void setVideoUrl(String videoUrl) { this.videoUrl = videoUrl; }
-}
